be22197bc468ea84a4286508bba470750d214284
Josh Brooker <jbtech85@gmail.com>
2025-11-02 21:43:04 -0800

    recipe list

--- frontend/src/pages/RecipeListPage.tsx
+++ frontend/src/pages/RecipeListPage.tsx
@@ -18,34 +18,43 @@ type responseListType {
 
 
 export async function recipeListLoader({ params }: paramsType) {
-  let filter
-    switch(params.filter) {
-      case 'categories':
-        filter = 'categories';
-      default:
-        filter = 'categories';
-    }
-  
+  let filter;
+  switch(params.filter) {
+    case 'categories':
+      filter = 'categories';
+    default:
+      filter = 'categories';
+  }  
 
-  let filterId = () => {
-    switch(params.filterId) {
-      default:
-        ''
-    }
-  }
+  let filterId;
+  switch(params.filterId) {
+    default:
+      ''
+  }  
 
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/${filter}${filterId}.php`) as responseListType;
 
+  // we're always setting this, so no default. handled in first switch case.
   switch(filter) {
     case 'categories':
       return (response.categories);
   }
 }
 
+
 export default function RecipeListPage() {
-  const recipeListInfo = useLoaderData();
+  const recipeList = useLoaderData();
+
 
   return (
-    <></>
+    <>
+      {recipeList.map((recipe: keyValType) => (
+        <div>
+          <h3>{recipe.strCategory}</h3>
+          <p>{recipe.strCategoryDescription}</p>
+          <a>{recipe.strCategory} recipes</a>
+        </div>
+      ))}
+    </>
   )
 }
