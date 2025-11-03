b791038d01946e5465926a22328f3d8bf3f5cd7a
Josh Brooker <jbtech85@gmail.com>
2025-11-02 21:30:30 -0800

    Recipe data calls

--- frontend/src/pages/RecipeListPage.tsx
+++ frontend/src/pages/RecipeListPage.tsx
@@ -1,10 +1,51 @@
+import { useParams, useLoaderData } from "react-router-dom";
+
+type paramsType = {
+  params: {
+    filter: string,
+    filterId: string | null;
+  }
+}
+
+type keyValType = {
+  [key: string]: string;
+}
+
+type responseListType {
+  categories?: keyValType[]
+}
+
+
+
 export async function recipeListLoader({ params }: paramsType) {
-  const response = await fetch('https://www.themealdb.com/api/json/v1/1/' + params.name);
-  return response;
+  let filter
+    switch(params.filter) {
+      case 'categories':
+        filter = 'categories';
+      default:
+        filter = 'categories';
+    }
+  
+
+  let filterId = () => {
+    switch(params.filterId) {
+      default:
+        ''
+    }
+  }
+
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/${filter}${filterId}.php`) as responseListType;
+
+  switch(filter) {
+    case 'categories':
+      return (response.categories);
+  }
 }
 
 export default function RecipeListPage() {
+  const recipeListInfo = useLoaderData();
+
   return (
-    <div>RecipeListPage</div>
+    <></>
   )
 }
