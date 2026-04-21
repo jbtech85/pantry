452b837434088f6b98081090bcbd9ab6a5122688
Josh Brooker <jbtech85@gmail.com>
2026-04-20 19:35:15 -0700

    GHA progress

---
+++ frontend/cypress/e2e/authFlow.cy.ts
@@ -0,0 +1,35 @@
+describe('Auth Flow', () => {
+  // using timestamps to achieve unique user names for later review
+  const timestamp = Date.now();
+  const testUserWithHousehold = {
+    email: `cy_house_user_${timestamp}@test.com`,
+    password: 'cool_password_bro',
+    createHousehold: true 
+  }
+
+  const testUserWithoutHousehold = {
+    email: `cy_nohouse_user_${timestamp}@test.com`,
+    password: 'hey_thanks_bro',
+    createHousehold: false
+  }
+
+
+  // failed login
+  it('fails to sign in via invalid credentials', () => {
+    cy.visit('/');
+    cy.contains('a','Login').click();
+
+    cy.get('input[type="email"]').type(testUserWithHousehold.email);
+    cy.get('input[type="password"').type("1234");
+    cy.get('button[type="submit"]').click();
+
+    
+  });
+
+});
+
+
+
+// create a user
+
+
